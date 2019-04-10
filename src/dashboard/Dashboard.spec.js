9896830c28dfa89cab11d1e005335ec72263ea25
Spencer Meredith <Smeredith1119@gmail.com>
2019-04-10 08:41:37 -0700

    mvp finish

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,24 @@
 // Test away
+
+
+import React from 'react';
+import Controls from '../controls/Controls';
+
+import { render, fireEvent } from 'react-testing-library';
+
+
+import Display from '../display/Display';
+
+import  'jest-dom/extend-expect';
+
+
+
+describe('<Dashboard />', () => {
+    
+    it('Display componenets part', () => {
+
+
+        expect().toEqual();
+
+    });
+}); 
